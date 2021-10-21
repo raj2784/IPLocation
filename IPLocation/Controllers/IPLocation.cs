fca99757b65f6dcdb466520fab60e5ce644ab732
Raj Bhatia <50281419+raj2784@users.noreply.github.com>
2021-10-21 15:29:30 +1030

    Add files via upload

---
+++ IPLocation/Controllers/IPLocation.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using IPLocation.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+
+
+namespace IPLocation.Controllers
+{
+    [Route("api/[controller]")]
+    public class IPLocation : ControllerBase
+    {
+        //[HttpGet("{ip}")]
+        public ActionResult<GeoLocationIP> GetUserLocationDetailsyByIp(string ip)
+        {
+            {
+                ip = "121.45.86.171";
+                GeoLocationIP ipInfo = new GeoLocationIP();
+                try
+                {                  
+                    var info = new WebClient().DownloadString($"https://ipapi.co/121.45.86.171/city/");
+                   
+                    if (info != null)
+                    {
+                        ipInfo.City = info;
+                                               
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+                return ipInfo;
+
+                
+            }
+        }
+    }
+}
---
+++ IPLocation/Model/GeoLocationIP.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace IPLocation.Model
+{
+    public class GeoLocationIP
+    {
+       
+
+       [JsonProperty("city")]
+        public string City { get; set; }
+
+    }
+}
